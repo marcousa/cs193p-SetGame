ea41f23e1ffe9de2bc749808b54aca53873230cc
Marco De Filippo <marcodefilippo@gmail.com>
2018-10-08 21:13:10 -0400

    Initial Commit

---
+++ SetGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SetGame
+//
+//  Created by Marco De Filippo on 10/8/18.
+//  Copyright © 2018 Marco De Filippo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
