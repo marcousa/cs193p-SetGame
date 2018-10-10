1853cdc2f9797770a0977df90bcc8b4d9a969338
Marco De Filippo <marcodefilippo@gmail.com>
2018-10-09 22:41:11 -0400

    Corrected the colors and symbols to be used for the cards in ViewController

--- SetGame/ViewController.swift
+++ SetGame/ViewController.swift
@@ -10,19 +10,79 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var deck = SetDeck()
+    private(set) var deck = SetDeck()
+    
+    private let shapes: [Int:String] = [
+        0: "●",
+        1: "▲",
+        2: "■"
+    ]
+    
+    private let colors: [Int:[CGFloat]] = [
+        0: [1,0,0],
+        1: [0,1,0],
+        2: [0,0,1]
+    ]
+    
+    @IBOutlet private var cardButtons: [UIButton]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        for _ in 0...9 {
+    }
+    
+    @IBAction private func drawThreeCards(_ sender: UIButton) {
+        for button in 0...2 {
             if let card = deck.draw() {
                 print(card)
+                
+                if let colorComponents = colors[card.color.rawValue] {
+                    
+                    var attributes = [NSAttributedString.Key:Any]()
+                    
+                    switch card.shading {
+                    case .striped:
+                        attributes = [
+                            .foregroundColor: UIColor(red: colorComponents[0], green: colorComponents[1], blue: colorComponents[2], alpha: 0.15),
+                            .strokeWidth: -3,
+                            .font: UIFont.systemFont(ofSize: 25)
+                        ]
+                    case .solid:
+                        attributes = [
+                            .strokeWidth: -3,
+                            .strokeColor: UIColor(red: colorComponents[0], green: colorComponents[1], blue: colorComponents[2], alpha: 1.0),
+                            .foregroundColor: UIColor(red: colorComponents[0], green: colorComponents[1], blue: colorComponents[2], alpha: 1.0),
+                            .font: UIFont.systemFont(ofSize: 25)
+                        ]
+                    case .outlined:
+                        attributes = [
+                            .strokeWidth: 3,
+                            .strokeColor: UIColor(red: colorComponents[0], green: colorComponents[1], blue: colorComponents[2], alpha: 1.0),
+                            .font: UIFont.systemFont(ofSize: 25)
+                        ]
+                    }
+                    
+                    let shape = shapes[card.shape.rawValue]
+                    var shapeText = ""
+                    
+                    for _ in 1...card.number.rawValue {
+                        shapeText += shape ?? ""
+                    }
+                    
+                    let attributedString = NSAttributedString(string: shapeText, attributes: attributes)
+                    
+                    cardButtons[button].setAttributedTitle(attributedString, for: .normal)
+                    
+                }
+                
+                
+                
+            } else {
+                print("No more cards!")
             }
         }
-        
     }
+    
 
 
 }
-
